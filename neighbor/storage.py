0d0a74a1691dfe3203184b929c8e6cd281db78f0
Luke Irvin <LukeGithub@outlook.com>
2021-02-08 10:17:31 -0700

    Rolled rowdata into returned object, pruned schema, namespaced sql artifacts into "orbitron" schema

--- neighbor/storage.py
+++ neighbor/storage.py
@@ -49,9 +49,9 @@ class Storage:
         tpls = [tuple(x) for x in nump]
     
         sql = """
-        INSERT INTO PostalCodeGeodata
-        (CountryCode,PostalCode,PlaceName,AdminName1,AdminCode1,AdminName2,AdminCode2,AdminName3,AdminCode3,Latitude,Longitude,Accuracy) 
-        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+        INSERT INTO Orbitron.PostalCodeGeodata
+        (PostalCode,Latitude,Longitude) 
+        VALUES(%s,%s,%s)"""
         cursor = self.connection.cursor()
         try:
             extras.execute_batch(cursor, sql, tpls, page_size)
@@ -60,13 +60,13 @@ class Storage:
             self.show_psycopg2_exception(err)
             cursor.close()
 
-    def execute_batch_neighborlocation_insert(self, dataframe, page_size=150):    
+    def execute_batch_neighborlocation_insert(self, dataframe, page_size=150):
         nump = dataframe.to_numpy()
 
         tpls = [tuple(x) for x in nump]
-    
+
         sql = """
-        INSERT INTO NeighborLocations
+        INSERT INTO Orbitron.NeighborLocations
         (SourceId, Name, Latitude, Longitude, RowData) 
         VALUES(%s,%s,%s,%s,%s)"""
         cursor = self.connection.cursor()
@@ -84,26 +84,21 @@ class Storage:
 
     def setup_tables(self):
         SQL = """
+        DROP SCHEMA IF EXISTS Orbitron CASCADE;
+
+        CREATE SCHEMA Orbitron;
+
         DROP TABLE IF EXISTS PostalCodeGeodata;
         DROP TABLE IF EXISTS NeighborLocations;
         DROP TABLE IF EXISTS Sources;
 
-        CREATE TABLE PostalCodeGeodata (
-            CountryCode char(2),
-            PostalCode varchar(20),
-            PlaceName varchar(180),
-            AdminName1 varchar(100),
-            AdminCode1 varchar(20),
-            AdminName2 varchar(100),
-            AdminCode2 varchar(20),
-            AdminName3 varchar(100),
-            AdminCode3 varchar(20),
-            Latitude Decimal(8,6),
-            Longitude Decimal(9,6),
-            Accuracy integer
+        CREATE TABLE Orbitron.PostalCodeGeodata (
+            PostalCode varchar(20) NOT NULL,
+            Latitude Decimal(8,6) NOT NULL,
+            Longitude Decimal(9,6) NOT NULL
         );
 		
-        CREATE TABLE Sources
+        CREATE TABLE Orbitron.Sources
         (
             Id SERIAL PRIMARY KEY,
             Name VARCHAR NOT NULL,
@@ -111,9 +106,9 @@ class Storage:
             UpdateDate DATE NOT NULL
         );
 
-        CREATE TABLE NeighborLocations
+        CREATE TABLE Orbitron.NeighborLocations
         (
-            Id SERIAL Primary Key,
+            Id SERIAL PRIMARY KEY,
             SourceId INTEGER NOT NULL,
             Name VARCHAR NOT NULL,
             Latitude DECIMAL(8,6) NOT NULL,
@@ -121,59 +116,55 @@ class Storage:
             RowData VARCHAR,
 			CONSTRAINT fk_NeighborLocations_Sources
                 FOREIGN KEY(SourceId) 
-	                REFERENCES Sources(Id)
+	                REFERENCES Orbitron.Sources(Id)
         );
 
         """
-		
-        #CREATE INDEX idx_NeighborLocations_SourceId ON NeighborLocations(SourceId)
+
         return self.execute(SQL)
-		
+
     def setup_postalcode_geometry(self):
         SQL = """
 		
-            SELECT AddGeometryColumn ('public','postalcodegeodata','Geometry',32611,'POINT',2); -- utm11_meters,
+            SELECT AddGeometryColumn ('orbitron','postalcodegeodata','Geometry',32611,'POINT',2); -- utm11_meters,
             
-            UPDATE PostalCodeGeodata SET "Geometry" = ST_Transform(ST_Centroid(ST_PointFromText('POINT(' || Longitude || ' ' || Latitude || ')', 4326)),32611);
+            UPDATE Orbitron.PostalCodeGeodata SET "Geometry" = ST_Transform(ST_Centroid(ST_PointFromText('POINT(' || Longitude || ' ' || Latitude || ')', 4326)),32611);
             
-            CREATE INDEX idx_PostalCodeGeodata_Geometry ON PostalCodeGeodata USING gist("Geometry");
+            CREATE INDEX idx_PostalCodeGeodata_Geometry ON Orbitron.PostalCodeGeodata USING gist("Geometry");
             
         """
         return self.execute(SQL)
 
     def setup_neighbor_geometry(self):
         SQL = """
-            ALTER TABLE NeighborLocations DROP COLUMN IF EXISTS "Geometry";
-            SELECT AddGeometryColumn ('public','neighborlocations','Geometry',32611,'POINT',2); -- utm11_meters,
+            ALTER TABLE Orbitron.NeighborLocations DROP COLUMN IF EXISTS "Geometry";
+            SELECT AddGeometryColumn ('orbitron','neighborlocations','Geometry',32611,'POINT',2); -- utm11_meters,
  
-            UPDATE NeighborLocations SET "Geometry" = ST_Transform(ST_Centroid(ST_PointFromText('POINT(' || Longitude || ' ' || Latitude || ')', 4326)),32611);
+            UPDATE Orbitron.NeighborLocations SET "Geometry" = ST_Transform(ST_Centroid(ST_PointFromText('POINT(' || Longitude || ' ' || Latitude || ')', 4326)),32611);
 			
-            CREATE INDEX idx_NeighborLocations_Geometry ON NeighborLocations USING gist("Geometry");
+            CREATE INDEX idx_NeighborLocations_Geometry ON Orbitron.NeighborLocations USING gist("Geometry");
     
-        """
-        #CREATE INDEX idx_NeighborLocations_GeometryAsGeography ON NeighborLocations USING gist("Geometry"::geography);    
+        """   
         return self.execute(SQL)
 
     def insert_source(self, sourceName):
-        
-	
         SQL = """
-            INSERT INTO Sources(Name, UpdateDate) VALUES (%(name)s, %(updateDate)s) RETURNING Id
+            INSERT INTO Orbitron.Sources(Name, UpdateDate) VALUES (%(name)s, %(updateDate)s) RETURNING Id
         """
         result = self.execute(SQL, {'name': sourceName, 'updateDate': datetime.datetime.today()})
         id = result.fetchone()["id"]
         return id
 
-    def get_neighbors_by_zip(self, count: int, sourceType: str, postalCode: str, distanceLimitInMiles: int = 1000):
+    def get_neighbors_by_zip(self, count: int, sourceType: str, postalCode: str, distanceLimitInMiles: int):
         # ST_Distance isn't index-aware, so use index-aware ST_DWithin with a distanceLimit 
         #  to prune the amount of data to sort
         distanceLimit = self.metersPerMile * distanceLimitInMiles
 
         SQL = """
             SELECT loc.Name, loc.Latitude, loc.Longitude, loc.RowData
-            FROM NeighborLocations AS loc
-            JOIN PostalCodeGeodata AS zip ON ST_DWithin(loc."Geometry", zip."Geometry", %(distanceLimit)s)
-            JOIN Sources AS src ON loc.SourceId = src.Id 
+            FROM Orbitron.NeighborLocations AS loc
+            JOIN Orbitron.PostalCodeGeodata AS zip ON ST_DWithin(loc."Geometry", zip."Geometry", %(distanceLimit)s)
+            JOIN Orbitron.Sources AS src ON loc.SourceId = src.Id 
             WHERE zip.PostalCode = %(postalCode)s AND src.Name = %(sourceType)s
             ORDER BY ST_Distance(loc."Geometry", zip."Geometry")
             FETCH FIRST %(count)s ROWS ONLY
@@ -186,11 +177,8 @@ class Storage:
 
         return neighbors
 
-
-
     def __enter__(self):
         return self
-
 
     def __exit__(self, exception_type, exception_value, tb):
         self.disconnect()
@@ -204,5 +192,6 @@ class Storage:
 
         log.error(f"psycopg2 ERROR: {err} on line number: {line_n}")
         log.error(f"psycopg2 traceback: {traceback} -- type: {err_type}")
+        log.error(f"psycopg2 error object: {err_obj}")
 
         
